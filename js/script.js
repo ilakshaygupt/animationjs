ef3c582442faa6ab975ce427db7c3d3297db236d
ilakshaygupt <gupta22154006@akgec.ac.in>
2023-10-01 23:13:01 +0530

    dynamic atoms

--- js/script.js
+++ js/script.js
@@ -25,9 +25,25 @@ function drawAtom(x, y, element) {
     ctx.fillText(element, x - 7, y + 7);
 }
 
+function moveAtoms() {
+    atoms.forEach(atom => {
+        atom.x += atom.velocityX;
+        atom.y += atom.velocityY;
+
+        // Bounce off the edges
+        if (atom.x - 25 < 0 || atom.x + 25 > canvas.width) {
+            atom.velocityX *= -1;
+        }
+        if (atom.y - 25 < 0 || atom.y + 25 > canvas.height) {
+            atom.velocityY *= -1;
+        }
+    });
+}
+
 //continuously draw
 function gameLoop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    moveAtoms();
     atoms.forEach(atom => {
         drawAtom(atom.x, atom.y, atom.element);
     });
